05758550c4202f1b42cca797ec80efdb567494f4
Linh Doan <thaolinh100798@gmail.com>
2024-04-26 09:18:27 +1000

    placement ok

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -23,36 +23,44 @@ export const SearchResults = () => {
 		}
 	];
   return (
-    <main>
-			<div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-					<ul className="flex flex-wrap -mb-px">
-							<li className="me-2">
-									<a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
-							</li>
-							<li className="me-2">
-									<a href="#" className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Dashboard</a>
-							</li>
-							<li className="me-2">
-									<a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Settings</a>
-							</li>
-							<li className="me-2">
-									<a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Contacts</a>
-							</li>
-							<li>
-									<a className="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
-							</li>
-					</ul>
-			</div>
-			<section className="py-7">
-				<p className="text-2xl text-gray-700 dark:text-white">{ results.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'` }</p>
-			</section>
-			<section className="py-7">
-				<div className="flex justify-start flex-wrap">       
-					{ results.map((item) => (
-					<CarouselItem key={item.id} imageUrl={item.imageUrl} />
-					)) }          
-				</div>
-			</section>
-    </main>
+    <main className="flex flex-col items-start w-full md:w-3/4 mx-auto sm:px-4 px-6">
+    <div className="text-sm font-medium text-left text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 w-full mt-4 overflow-x-auto overflow-y-hidden">
+        <ul className="flex space-x-4 -mb-px">
+            <li>
+                <button type="button"  className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">All</button>
+            </li>
+            <li>
+                <button type="button" className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Books</button>
+            </li>
+            <li>
+                <button type="button" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Chapters</button>
+            </li>
+            <li>
+                <button type="button" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Genres</button>
+            </li>
+            <li>
+                <button type="button" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">People</button>
+            </li>
+        </ul>
+    </div>
+
+    <section className="py-7 w-full">
+        <p className="text-2xl text-gray-700 dark:text-white">{ results.length === 0 ? `No result found for '${queryTerm}'` : `Results for '${queryTerm}'` }</p>
+    </section>
+
+    <section className="py-7 w-full">
+        <div className="flex flex-wrap gap-4">       
+            { results.map((item) => (
+                <CarouselItem key={item.id} imageUrl={item.imageUrl} />
+            )) }          
+        </div>
+    </section>
+</main>
+
+
+
+
+
+
   )
 }
