11747d774cd2187c8d76e8079a2ce30b75510e34
Linh Doan <thaolinh100798@gmail.com>
2024-04-25 17:34:37 +1000

    small fixes

--- src/pages/Home/components/ImageCard.js
+++ src/pages/Home/components/ImageCard.js
@@ -5,7 +5,7 @@ export const ImageCard = ( {source, children}) => {
 
     <figure className="relative mx-auto">
     <a href="#">
-        <img className="rounded-lg h-[50vh] w-full object-cover" src={source} alt="image description"/>
+        <img className="h-[50vh] w-full object-cover" src={source} alt="image description"/>
     </a>
     <figcaption className="absolute h-[50vh] flex items-center justify-start w-full top-0 left-0 lg:left-1/4">
         <div className="text-left px-4">
