339f58bbfa72fecbeea88ee7fc13fdb9b443b7a9
Linh Doan <thaolinh100798@gmail.com>
2024-04-25 11:10:50 +1000

    use different slider component

--- src/pages/Home/components/FeaturedCarousel.js
+++ src/pages/Home/components/FeaturedCarousel.js
@@ -1,22 +1,30 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import Book1 from "../../../assets/book1.jpeg";
 import Book2 from "../../../assets/book2.avif";
 import Book3 from "../../../assets/book3.jpeg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { ImageCard } from "./ImageCard";
 export const FeaturedCarousel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
   return (
     <div className="bg-gray-100 dark:bg-gray-800">
-      <Carousel autoPlay showThumbs={false} showStatus={false} infiniteLoop={true}>
+      <Slider {...settings}>
         <div>
-          <img src={Book1} alt="Introduction" style={{height:"60vh", width: "fit-content"}}/>
+          <ImageCard/>
         </div>
         <div>
-          <img src={Book2} alt="Writer" style={{height:"60vh", width: "fit-content"}}/>
+          <ImageCard />
         </div>
         <div>
-          <img src={Book3} alt="Reader" style={{height:"60vh", width: "fit-content"}}/>
+          <ImageCard />
         </div>
-      </Carousel>
+      </Slider>
     </div>
     
     
