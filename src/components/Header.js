19b6157da4dc5d257a04aafd98a07713951d0183
Linh Doan <thaolinh100798@gmail.com>
2024-04-26 23:08:33 +1000

    state of search results created

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ export const Header = () => {
   const [hidden, setHidden] = useState(true)
   const navigate = useNavigate()
 
-  const activeClass = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-  const inActiveClass = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+  const activeClass = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
+  const inActiveClass = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
   
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -26,15 +26,15 @@ export const Header = () => {
 
   return (
     <header>
-      <nav className="bg-gray-50 dark:bg-gray-800 border-gray-200 dark:bg-gray-900">
+      <nav className="bg-gray-50 border-gray-200">
         <div className="flex flex-wrap items-center justify-between mx-auto p-4 px-6">
           <section className="flex items-center justify-between">
             <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Universal Book Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Universal Book</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap">Universal Book</span>
             </Link>
             <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 ps-6" id="navbar-search">
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-50 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-50">
                 <li>
                   <NavLink to="/" className={({isActive}) => isActive ? activeClass : inActiveClass}>Home</NavLink>
                 </li>
@@ -53,7 +53,7 @@ export const Header = () => {
           
           <div className="flex items-center justify-between">
             {/* Search icon small screen */}
-            <button onClick={() => setHidden(!hidden)} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+            <button onClick={() => setHidden(!hidden)} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-black  hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 rounded-lg text-sm p-2.5 me-1">
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
@@ -63,19 +63,19 @@ export const Header = () => {
             {/* Search full screen */}
             <div className="relative hidden md:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-black dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <svg className="w-4 h-4 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
                 <span className="sr-only">Search icon</span>
               </div>
               <form onSubmit={handleSubmit}>
-                <input type="text" id="search-navbar" name="search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+                <input type="text" id="search-navbar" name="search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..."/>
               </form>
               
             </div>
-            <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white px-3"></span>
+            <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 px-3"></span>
             {/* Burger icon small screen */}
-            <button onClick={() => setHidden(!hidden)} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+            <button onClick={() => setHidden(!hidden)} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-search" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -86,15 +86,15 @@ export const Header = () => {
           <div className={`items-center ${hidden? "hidden" : ""} justify-between w-full md:hidden md:w-auto md:order-1 ps-6" id="navbar-search`}>
             <div className="relative mt-3">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
               </div>
               <form onSubmit={handleSubmitChangeState}>
-                <input type="text" name="search" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+                <input type="text" name="search" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-200 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..."/>
               </form>
             </div>
-            <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-200 rtl:space-x-reverse dark:bg-gray-800 dark:border-gray-700">
+            <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-200 rtl:space-x-reverse">
               <li>
                 <NavLink to="/" className={({isActive}) => isActive ? activeClass : inActiveClass}>Home</NavLink>
               </li>
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,61 +1,115 @@
-import React from 'react'
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { CarouselItem } from '../components'
 import Book1 from "../assets/book1.jpeg";
 import Book2 from "../assets/book2.avif";
 import Book3 from "../assets/book3.jpeg";
 export const SearchResults = () => {
+	const [activeTab, setActiveTab] = useState(0);
 	const [searchParams] = useSearchParams()
 	const queryTerm = searchParams.get("q")
+	const tabs = [
+		// {
+		// 	tabDisplay: 'All',
+		// 	patternMatching: /.*.*/
+		// },
+		{
+			tabDisplay: 'Books',
+			patternMatching: /^Book$/
+		},
+		{
+			tabDisplay: 'Chapters',
+			patternMatching: /^Chapter$/
+		},
+		{
+			tabDisplay: 'Genres',
+			patternMatching: /^Genre$/
+		},
+		{
+			tabDisplay: 'People',
+			patternMatching: /^People$/
+		}
+	]
+	const inactiveClass = "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+	const activeClass = "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active"
 	// API call
 	const results = [  
 		{
 			id: 1,
-			imageUrl: Book1, 
+			imageUrl: Book1,
+			category: "Book"
 		},
 		{
 			id: 2,
-			imageUrl: Book2, 
+			imageUrl: Book1, 
+			category: "Book"
 		},
 		{
 			id: 3,
+			imageUrl: Book1, 
+			category: "Book"
+		},
+		{
+			id: 4,
+			imageUrl: Book2,
+			category: "Chapter"
+		},
+		{
+			id: 5,
+			imageUrl: Book2, 
+			category: "Chapter"
+		},
+		{
+			id: 6,
+			imageUrl: Book2, 
+			category: "Chapter"
+		},
+		{
+			id: 7,
+			imageUrl: Book3,
+			category: "People"
+		},
+		{
+			id: 8,
 			imageUrl: Book3, 
+			category: "People"
+		},
+		{
+			id: 9,
+			imageUrl: Book3, 
+			category: "People"
 		}
 	];
   return (
     <main className="flex flex-col items-start w-full md:w-3/4 mx-auto sm:px-4 px-6">
-    <div className="text-sm font-medium text-left text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 w-full mt-4 overflow-x-auto overflow-y-hidden">
+    <div className="text-sm font-medium text-left text-gray-500 border-b border-gray-200 w-full mt-4 overflow-x-auto overflow-y-hidden">
         <ul className="flex space-x-4 -mb-px">
-            <li>
-                <button type="button"  className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">All</button>
-            </li>
-            <li>
-                <button type="button" className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Books</button>
-            </li>
-            <li>
-                <button type="button" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Chapters</button>
-            </li>
-            <li>
-                <button type="button" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Genres</button>
-            </li>
-            <li>
-                <button type="button" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">People</button>
-            </li>
+					{tabs.map((item, index) => (
+						<li key={item.tabDisplay}>
+							<button 
+								type="button"  
+								className={index === activeTab ? activeClass : inactiveClass}
+								onClick={() => setActiveTab(index)}
+							>
+								{item.tabDisplay}
+							</button>
+						</li>
+        ))}
         </ul>
     </div>
 
     <section className="py-7 w-full">
-        <p className="text-2xl text-gray-700 dark:text-white">{ results.length === 0 ? `No result found for '${queryTerm}'` : `Results for '${queryTerm}'` }</p>
+        <p className="text-2xl text-gray-700">{ results.filter((item) => item.category.match(tabs[activeTab].patternMatching)).length === 0 ? `No ${tabs[activeTab].tabDisplay.toLowerCase()} result found for '${queryTerm}'` : `${tabs[activeTab].tabDisplay} results for '${queryTerm}'` }</p>
     </section>
 
     <section className="py-7 w-full">
         <div className="flex flex-wrap gap-4">       
-            { results.map((item) => (
+            { results.filter((item) => item.category.match(tabs[activeTab].patternMatching)).map((item) => (
                 <CarouselItem key={item.id} imageUrl={item.imageUrl} />
             )) }          
         </div>
     </section>
-</main>
+	</main>
 
 
 
