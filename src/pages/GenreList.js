aca82d42bc5dfcd01ed297f789d086a8a328afb4
Mingxuan Fu <mfuu0003@student.monash.edu>
2024-04-27 13:43:59 +1000

    improved carousel

--- src/pages/GenreList.js
+++ src/pages/GenreList.js
@@ -1,9 +1,45 @@
 import { CustomMultiCarousel } from "./Home/components/BookMultiCarousel.js"
+import Book1 from "../assets/book1.jpeg";
+
+const genres = [
+    {
+      id: 1,
+      title: "test genre",
+      imageUrl: Book1, 
+    },
+    {
+      id: 2,
+      title: "test genre",
+      imageUrl: Book1, 
+    },
+    {
+      id: 3,
+      title: "test genre",
+      imageUrl: Book1, 
+    },
+    {
+      id: 4,
+      title: "test genre",
+      imageUrl: Book1, 
+    },
+    {
+      id: 5,
+      title: "test genre",
+      imageUrl: Book1, 
+    },
+    {
+      id: 6,
+      title: "test genre",
+      imageUrl: Book1, 
+    },
+];
 
 export const GenreList = () => {
   return (
     <main>
-      <CustomMultiCarousel title={"test"} itemsToDisplay={[]}/>
+      <CustomMultiCarousel title={"Hot Genres"} itemsToDisplay={genres}/>
+      <CustomMultiCarousel title={"Top Genres"} itemsToDisplay={[]}/>
+      <CustomMultiCarousel title={"New Genres"} itemsToDisplay={[]}/>
     </main>
   )
 }
--- src/pages/Home/components/BookMultiCarousel.js
+++ src/pages/Home/components/BookMultiCarousel.js
@@ -1,37 +1,35 @@
 import MultiCarousel from "react-multi-carousel";
+import { CarouselItem } from "./CarouselItem";
 
 import "react-multi-carousel/lib/styles.css";
 const responsive = {
     superLargeDesktop: {
         // the naming can be any, depends on you.
         breakpoint: {max: 4000, min: 3000},
-        items: 5
+        items: 7
     },
     desktop: {
         breakpoint: {max: 3000, min: 1024},
-        items: 3
+        items: 6
     },
     tablet: {
         breakpoint: {max: 1024, min: 464},
-        items: 2
+        items: 4
     },
     mobile: {
         breakpoint: {max: 464, min: 0},
-        items: 1
+        items: 3
     }
 };
 
 export const CustomMultiCarousel = ({title, itemsToDisplay}) => {
     return (
-        <>
-            <h1> {title} </h1>
-            <MultiCarousel responsive={responsive}>
-                <div>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
+        <div>
+            <h1 className="text-xl font-semibold px-3 py-1 justify-center"> {title} </h1>
+            <MultiCarousel responsive={responsive} className="px-3 py-1">
+                {itemsToDisplay.map((obj, idx) => { return <CarouselItem title={obj.title} imageUrl={obj.imageUrl}/> } )}
             </MultiCarousel> 
-        </>
+        </div>
         
     );
 }
