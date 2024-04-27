27b90cfacdf7ffb1054a72461b30171ed25bcb4e
echooff16 <aritro.dutta16@gmail.com>
2024-04-27 21:54:20 +1000

    fixing minor changes on login page (sign up to login button)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -60,7 +60,7 @@ export const Login = () => {
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
               >
-                Sign In
+                Login
               </button>
               <a
                 className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
