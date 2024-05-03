d0aca9434efde74978a47ea165e6499c1416c122
Linh Doan <thaolinh100798@gmail.com>
2024-05-04 09:36:12 +1000

    edit profile functionality added, active tab fixed

--- src/pages/Dashboard/Components/Profile.js
+++ src/pages/Dashboard/Components/Profile.js
@@ -1,29 +1,97 @@
+import { useState, useEffect } from "react";
 export const Profile = () => {
-  return (
-    <div className="px-20">
-      <h1 className="px-3 py-4 text-3xl font-bold">Profile</h1>
-      <h2 className="px-3 py-4 text-large">Personal information</h2>
-      <div className="w-full flex flex-row items-start justify-between p-6 bg-white border border-gray-200 rounded-lg shadow">
-        <div>
-          <p className="mb-3 font-normal text-gray-700">First name: Bonnie</p>
-					<p className="mb-3 font-normal text-gray-700">Last name: Green</p>
-					<p className="mb-3 font-normal text-gray-700">DOB: 1/1/2000</p>
-					<p className="mb-3 font-normal text-gray-700">Address: 1 High Str, Melbourne</p>
-					<p className="mb-3 font-normal text-gray-700">Religion: None</p>
-        </div>
-        <div class="flex justify-end px-4 pt-4">
-          <button id="dropdownButton" data-dropdown-toggle="dropdown" class=" inline-block text-gray-500 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg text-sm p-1.5" type="button">
-              <span class="sr-only">Open dropdown</span>
-                  <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                      <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd"/>
-                      <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd"/>
-              </svg>
+  const [editing, setEditing] = useState(false);
+  const [profile, setProfile] = useState({});
+  const [cachedProfile, setCachedProfile] = useState({})
+  useEffect(() => {
+    //Replace with call to backend
+    setProfile({
+      firstName: 'Bonnie',
+      lastName: 'Green',
+      dob: '2000-01-01',
+      email: 'bonnie.green@email.com',
+      phoneNumber: '041111111',
+      religion: null
+    })
+    setCachedProfile(profile)
+  }, []);
 
-          </button>
-          
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+    //send data to DB
+    setCachedProfile(profile)
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+    //Alternatively can make another DB call
+    setProfile(cachedProfile)
+  };
+
+  const handleChange = (e) => {
+    setProfile({ ...profile, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="px-48">
+      <h1 className="py-4 text-3xl font-bold">Profile</h1>
+      <h2 className="py-4 text-large">Personal information</h2>
+      {editing ? (
+        <form className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
+          <div className="relative z-0 mb-5 group">
+            <input type="text" name="firstName" value={profile.firstName} onChange={handleChange} id="firstName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label for="firstName" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+          </div>
+          <div className="relative z-0 mb-5 group">
+            <input type="text" name="lastName" value={profile.lastName} onChange={handleChange} id="lastName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label for="lastName" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+          </div>
+          <div className="relative z-0 mb-5 group">
+            <input type="date" name="dob" value={profile.dob} onChange={handleChange} id="dob" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label for="dob" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date of birth</label>
+          </div>
+          <div className="relative z-0 mb-5 group">
+              <input type="email" name="email" value={profile.email} onChange={handleChange} id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label for="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+          </div>
+          <div className="relative z-0 mb-5 group">
+              <input type="tel" pattern="[0-9]{10}" name="phoneNumber" value={profile.phoneNumber} onChange={handleChange} id="phoneNumber" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label for="phoneNumber" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (1234567890)</label>
+          </div>
+          <div className="relative z-0 mb-5 group">
+              <input type="text" name="religion" value={profile.religion} onChange={handleChange} id="religion" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label for="religion" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Religion</label>
+          </div>
+          <button type="submit" onClick={handleSave} className="text-white mr-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+          <button type="submit" onClick={handleCancel} className="text-white ml-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+        </form>
+      ) : (
+        <div className="w-full flex flex-row items-start justify-between p-6 bg-white border border-gray-200 rounded-lg shadow">
+          <div>
+            <p className="mb-3 font-normal text-gray-700">First name: {profile.firstName}</p>
+            <p className="mb-3 font-normal text-gray-700">Last name: {profile.lastName}</p>
+            <p className="mb-3 font-normal text-gray-700">DOB: {profile.dob}</p>
+            <p className="mb-3 font-normal text-gray-700">Email: {profile.email}</p>
+            <p className="mb-3 font-normal text-gray-700">Phone number: {profile.phoneNumber}</p>
+            <p className="mb-3 font-normal text-gray-700">Religion: {profile.religion}</p>
+          </div>
+          <div className="flex justify-end px-4 pt-4">
+            <button id="editButton"  onClick={handleEditClick} className=" inline-block text-gray-500 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg text-sm p-1.5" type="button">
+              <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fillRule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clipRule="evenodd"/>
+                <path fillRule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clipRule="evenodd"/>
+              </svg>
+            </button>
+          </div>
         </div>
-        
-      </div>
+      )}
     </div>
-  )
+
+    
+  );
 }
--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -1,15 +1,21 @@
-import {  Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+import {  Link, Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const DashboardPage= () => {
-  const [activeTab, setActiveTab] = useState("dashboard");
-  // const activeClass = "flex items-center p-2 text-gray-900 rounded-lg bg-gray-100 group"
+  const [activeTab, setActiveTab] = useState("");
+  const location = useLocation();
+  const pathSplit = location.pathname.split('/')
+  const path = pathSplit[pathSplit.length-1]
+  useEffect(() => {
+    setActiveTab(path)
+  }, [path]);
+  
   return (
     <main>
       <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
         <span className="sr-only">Open sidebar</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
       </button>
       <section className="flex flex-row h-full w-full">
@@ -17,25 +23,25 @@ export const DashboardPage= () => {
           <div className="h-full px-3 py-4 overflow-y-auto">
             <ul className="space-y-2 font-medium">
               <li>
-                <Link to="/dashboard" onClick={() => setActiveTab('dashboard')} className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'dashboard'? '' : 'hover:' }bg-gray-100 group`}>
+                <Link to="/dashboard" className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'dashboard'? '' : 'hover:' }bg-gray-100 group`}>
                   <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
+                    <path fillRule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clipRule="evenodd"/>
                   </svg>
 
                   <span className="ms-3">Dashboard</span>
                 </Link>
               </li>
               <li>
-                <Link to="profile" onClick={() => setActiveTab('profile')}  className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'profile'? '' : 'hover:' }bg-gray-100 group`}>
+                <Link to="profile" className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'profile'? '' : 'hover:' }bg-gray-100 group`}>
                   <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clip-rule="evenodd"/>
+                    <path fillRule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clipRule="evenodd"/>
                   </svg>
 
                   <span className="flex-1 ms-3 whitespace-nowrap">Profile</span>
                 </Link>
               </li>
               <li>
-                <Link to="#" onClick={() => setActiveTab('create book')}  className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'create book'? '' : 'hover:'}bg-gray-100 group`}>
+                <Link to="#" className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'create book'? '' : 'hover:'}bg-gray-100 group`}>
                 <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
                 </svg>
@@ -43,7 +49,7 @@ export const DashboardPage= () => {
                 </Link>
               </li>
               <li>
-                <Link to="membership" onClick={() => setActiveTab('membership')}  className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'membership'? '' : 'hover:'}bg-gray-100 group`}>
+                <Link to="membership" className={`flex items-center p-2 text-gray-900 hover:rounded-lg ${activeTab === 'membership'? '' : 'hover:'}bg-gray-100 group`}>
                 <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"/>
                 </svg>
