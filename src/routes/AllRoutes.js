ce24dd12ee5f2983a2ea3fbe7f208886bb2500ad
Paarth Sharma <psha0064@student.monash.edu>
2024-05-19 23:06:26 +1000

    create chapter

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,30 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-
-import { HomePage, GenreList, BookList, ChapterList, Login, Register, PageNotFound, SearchResults, DashboardPage, BookDetails, Dashboard, Profile, MembershipDetails } from "../pages";
-
+import { HomePage, GenreList, BookList, ChapterList, Login, Register, PageNotFound, SearchResults, DashboardPage, BookDetails, Dashboard, Profile, MembershipDetails, CreateChapter } from "../pages";
 
 export const AllRoutes = () => {
   return (
     <main>
         <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="genres" element={<GenreList />}></Route>
-            <Route path="books" element={<BookList />}></Route>
-            <Route path="chapters" element={<ChapterList />}></Route>
-            <Route path="login" element={<Login />}></Route>
-            <Route path="register" element={<Register />}></Route>
-            <Route path="search" element={<SearchResults />}></Route>
-
+            <Route path="/" element={<HomePage />} />
+            <Route path="genres" element={<GenreList />} />
+            <Route path="books" element={<BookList />} />
+            <Route path="chapters" element={<ChapterList />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="search" element={<SearchResults />} />
             <Route path="bookdetails/:id" element={<BookDetails />} />
-
+            <Route path="create-chapter" element={<CreateChapter />} />
             <Route path="dashboard" element={<DashboardPage />}>
               <Route index element={<Dashboard />} />
               <Route path="profile" element={<Profile />} />
               <Route path="membership" element={<MembershipDetails />} />
-              </Route>
-            <Route path="*" element={<PageNotFound />}></Route>
-
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     </main>
-  )
-}
+  );
+};
