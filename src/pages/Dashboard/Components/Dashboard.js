533cd98ce168abf6d4c324e386f9be7e6641e513
echooff16 <aritro.dutta16@gmail.com>
2024-06-17 10:02:01 +0530

    fixed the redundancies in the code

--- src/pages/Dashboard/Components/Dashboard.js
+++ src/pages/Dashboard/Components/Dashboard.js
@@ -1,23 +1,6 @@
 import {  Link } from "react-router-dom";
 import UserProfile from "../../../assets/userProfile.png"
-import FeaturedSlider from "../../../components/FeaturedSlider";
-import Book4 from "../../../assets/book4.jpg";
-import Book5 from "../../../assets/book5.jpg";
-import Book6 from "../../../assets/book6.jpg";
-const books = [
-	{
-		id: 1,
-		imageUrl: Book4, 
-	},
-	{
-		id: 2,
-		imageUrl: Book5, 
-	},
-	{
-		id: 3,
-		imageUrl: Book6, 
-	}
-];
+
 export const Dashboard = () => {
   return (
     <>
@@ -29,8 +12,6 @@ export const Dashboard = () => {
 						<span className="text-sm text-gray-500">3.9M followers • 155 following</span>
 					</div>
 					<div className="flex flex-row mt-4 md:mt-6 pr-10">
-						{/* <Link to="#" className="inline-flex items-center justify-center px-4 py-2 ml-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Follow</Link>
-						<Link to="#" className="inline-flex items-center justify-center px-4 py-2 ml-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Message</Link> */}
 						<Link to="#" className="inline-flex items-center justify-center px-4 py-2 ml-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Edit intro</Link>
 					</div>
 			</div>
