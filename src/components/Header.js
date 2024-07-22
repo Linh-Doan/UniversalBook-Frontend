57d0972b6d575c16f9c934fd47e5cf04ce0005a3
echooff16 <aritro.dutta16@gmail.com>
2024-07-22 22:39:51 +1000

    roll back header as in main branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { useState, useEffect, useRef } from "react";
-import { NavLink, Link, useNavigate,  useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { NavLink, Link, useNavigate, useLocation } from "react-router-dom";
 import UserProfile from "../assets/userProfile.png"; // Adjust the import path as needed
 
 export const Header = () => {
@@ -8,23 +8,10 @@ export const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true); // Replace with actual login state
   const [showConfirmLogout, setShowConfirmLogout] = useState(false);
   const navigate = useNavigate();
-  const dropdownRef = useRef(null);
+  const location = useLocation();
 
   const activeClass = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0";
   const inActiveClass = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0";
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdownVisible(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -59,6 +46,15 @@ export const Header = () => {
   const cancelLogout = () => {
     setShowConfirmLogout(false);
   };
+
+  const handleNavigation = (path) => {
+    setDropdownVisible(false);
+    navigate(path);
+  };
+
+  useEffect(() => {
+    setDropdownVisible(false);
+  }, [location]);
 
   return (
     <header>
@@ -108,7 +104,7 @@ export const Header = () => {
                 <input type="text" id="search-navbar" name="search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." />
               </form>
             </div>
-            <div className="relative" ref={dropdownRef}>
+            <div className="relative">
               <button onClick={toggleDropdown} className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 px-3"></button>
               {dropdownVisible && (
                 <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-50">
@@ -118,16 +114,14 @@ export const Header = () => {
                         <span className="text-gray-700">Bonnie Green</span>
                         <img src={UserProfile} alt="User Profile" className="h-8 w-8 rounded-full" />
                       </div>
-                      <button onClick={() => navigate('/dashboard/profile')} className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Profile</button>
-                      <button onClick={() => navigate('/dashboard')} className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Dashboard</button>
-                      <button onClick={() => navigate('/dashboard/membership')} className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Membership Details</button>
+                      <button onClick={() => handleNavigation('/profile/1')} className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Dashboard</button>
                       <button onClick={handleLogout} className="text-blue-700 font-semibold text-left hover:bg-gray-200">Logout</button>
                     </div>
                   ) : (
                     <div className="flex flex-col p-2 text-left">
                       <span className="text-gray-700 mb-2">Not logged in yet</span>
-                      <button onClick={() => navigate('/login')} className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Login</button>
-                      <button onClick={() => navigate('/register')} className="text-blue-700 font-semibold text-left hover:bg-gray-200">New here?</button>
+                      <button onClick={() => handleNavigation('/login')} className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Login</button>
+                      <button onClick={() => handleNavigation('/register')} className="text-blue-700 font-semibold text-left hover:bg-gray-200">New here?</button>
                     </div>
                   )}
                 </div>
@@ -141,32 +135,43 @@ export const Header = () => {
               </svg>
             </button>
           </div>
-        </div>
-        {/* Search on small screen */}
-        {!hidden && (
-          <div className="w-full block md:hidden md:w-auto" id="navbar-search">
-            <div className="p-4 border border-gray-100 rounded-lg bg-gray-50">
+          {/* Search field small screen */}
+          <div className={`items-center ${hidden ? "hidden" : ""} justify-between w-full md:hidden md:w-auto md:order-1 ps-6" id="navbar-search`}>
+            <div className="relative mt-3">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </div>
               <form onSubmit={handleSubmitChangeState}>
-                <div className="relative mt-1">
-                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
-                  </div>
-                  <input type="text" id="search-navbar" name="search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." />
-                </div>
+                <input type="text" name="search" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-200 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." />
               </form>
             </div>
+            <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-200 rtl:space-x-reverse">
+              <li>
+                <NavLink to="/" className={({ isActive }) => isActive ? activeClass : inActiveClass}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/genres" className={({ isActive }) => isActive ? activeClass : inActiveClass}>Genres</NavLink>
+              </li>
+              <li>
+                <NavLink to="/books" className={({ isActive }) => isActive ? activeClass : inActiveClass}>Books</NavLink>
+              </li>
+              <li>
+                <NavLink to="/chapters" className={({ isActive }) => isActive ? activeClass : inActiveClass}>Chapters</NavLink>
+              </li>
+            </ul>
           </div>
-        )}
+        </div>
       </nav>
+      {/* Confirmation Dialog */}
       {showConfirmLogout && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-4 rounded-lg shadow-lg">
-            <p>Are you sure you want to logout?</p>
-            <div className="flex justify-end space-x-2 mt-4">
-              <button onClick={confirmLogout} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Yes</button>
-              <button onClick={cancelLogout} className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">No</button>
+          <div className="bg-white p-4 rounded-lg shadow-lg text-center z-60">
+            <p className="mb-4">Are you sure you want to logout?</p>
+            <div className="flex justify-around">
+              <button onClick={confirmLogout} className="bg-red-500 text-white px-4 py-2 rounded">Yes</button>
+              <button onClick={cancelLogout} className="bg-gray-300 text-black px-4 py-2 rounded">No</button>
             </div>
           </div>
         </div>
