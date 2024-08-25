3a909d1c72f1558b1a4ce7809769d8b3a85a0622
KhoaNguyenAn <nguyenankhoa1402@gmail.com>
2024-08-25 22:19:16 +1000

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 const apiBaseUrlRoot = process.env.REACT_APP_API_BASE_URL_ROOT;
+
 const endpoints = {
   getBooks: '/books',
   getUsers: '/users',
@@ -8,7 +9,13 @@ const endpoints = {
   getNewBooks: '/books/latest',
   followBook: '/follow/book',
   bookComments: '/comment/book',
-  genres: '/genres'
+  genres: '/genres',
+  getTopRatedGenres: '/genres/top-rated',  // Endpoint to get top-rated genres
+  getLatestGenres: '/genres/latest',       // Endpoint to get latest genres
+  getTrendingGenres: '/genres/trending',   // Endpoint to get trending genres
+  getDiscoverGenres: '/genres/discover',   // Endpoint to get discover genres
+  getMostFollowedGenres: '/genres/most-followed',  // Endpoint to get most-followed genres
+  isUserFollowingGenre: '/genres/is-following', // Endpoint to check if a user is following a specific genre
   // add more endpoints here
 };
 
