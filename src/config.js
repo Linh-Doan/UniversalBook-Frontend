d96b86d75b051924a9a58a3a1dfca5695607ef31
echooff16 <aritro.dutta16@gmail.com>
2024-08-26 16:10:56 +1000

    fixed config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,20 @@ const apiBaseUrlRoot = process.env.REACT_APP_API_BASE_URL_ROOT;
 
 const endpoints = {
   getBooks: '/books',
+  getUsers: '/users',
+  authorGroup: '/authorgroup',
   getTopRatedBooks: '/books/top-rated',
   getNewBooks: '/books/latest',
+  followBook: '/follow/book',
+  bookComments: '/comment/book',
+  genres: '/genres',
+  getTopRatedGenres: '/genres/top-rated',  // Endpoint to get top-rated genres
+  getLatestGenres: '/genres/latest',       // Endpoint to get latest genres
+  getTrendingGenres: '/genres/trending',   // Endpoint to get trending genres
+  getDiscoverGenres: '/genres/discover',   // Endpoint to get discover genres
+  getMostFollowedGenres: '/genres/most-followed',  // Endpoint to get most-followed genres
+  isUserFollowingGenre: '/genres/is-following', // Endpoint to check if a user is following a specific genre
+  // add more endpoints here
   getChapters: '/chapters',  
   getTopRatedChapters: '/chapters/top-rated', 
   getNewChapters: '/chapters/latest'  
