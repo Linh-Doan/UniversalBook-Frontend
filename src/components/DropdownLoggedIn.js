d430ab331c025696ec84168ce09e8d531171977c
Linh Doan <thaolinh100798@gmail.com>
2024-09-01 18:47:57 +1000

    connect to backend

---
+++ src/components/DropdownLoggedIn.js
@@ -0,0 +1,43 @@
+
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import UserProfile from "../assets/userProfile.png";
+import { logout } from "../services/authService";
+export const DropdownLoggedIn = ({setDropdownVisible, currentUser}) => {
+    const [showConfirmLogout, setShowConfirmLogout] = useState(false);
+    const navigate = useNavigate();
+
+    const logoutNavigate = () => {
+        logout();
+        navigate('/');
+    }
+
+    const confirmLogout = () => {
+        setShowConfirmLogout(false);
+        logoutNavigate();
+        
+    };
+    return (
+        <div>
+            <div className="flex flex-col p-2 text-left">
+                <div className="flex items-center justify-between mb-2">
+                    <span className="text-gray-700">{currentUser.account_name}</span>
+                    <img src={UserProfile} alt="User Profile" className="h-8 w-8 rounded-full" />
+                </div>
+                <Link onClick={() => setDropdownVisible(false)} to="/profile/1" className="text-blue-700 font-semibold mb-2 text-left hover:bg-gray-200">Dashboard</Link>
+                <button onClick={() => setShowConfirmLogout(true)} className="text-blue-700 font-semibold text-left hover:bg-gray-200">Logout</button>
+            </div>
+            {showConfirmLogout && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                <div className="bg-white p-4 rounded-lg shadow-lg text-center z-60">
+                    <p className="mb-4">Are you sure you want to logout?</p>
+                    <div className="flex justify-around">
+                    <button onClick={confirmLogout} className="bg-red-500 text-white px-4 py-2 rounded">Yes</button>
+                    <button onClick={() => setShowConfirmLogout(false)} className="bg-gray-300 text-black px-4 py-2 rounded">No</button>
+                    </div>
+                </div>
+            </div>
+            )}
+        </div>
+    )
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,72 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import viewIcon from '../assets/view.png'; // Import the view icon image
 import hiddenIcon from '../assets/hidden.png'; // Import the hidden icon image
 import background_img from '../assets/login_page.jpg';
+import { apiBaseUrl, endpoints } from '../config';
+import axiosInstance from '../api/axiosInstance';
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+});
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const loginDetails = {
+      email: formData.email,
+      account_password: formData.password
+    }
+    try {
+      await axiosInstance.post(`${apiBaseUrl}${endpoints.login}`, 
+      loginDetails,
+      {
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+      });
+      navigate('/');
+    } catch (err) {
+      if (err.name === "AxiosError"){
+        alert(err.response.data.message);
+      }
+    }
 
+  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+        ...formData,
+        [name]: value
+    });
+  }
   return (
     <main> 
       <div
         className="min-h-screen flex items-center justify-center bg-cover"
         style={{ backgroundImage: `url(${background_img})` }}
-
       >
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <h2 className="text-2xl mb-6">Login</h2>
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                Username
+                Email
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
+                id="email"
                 type="text"
-                placeholder="Username"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
             <div className="mb-6">
@@ -41,6 +79,9 @@ export const Login = () => {
                   id="password"
                   type={showPassword ? "text" : "password"}
                   placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
                 />
                 <button
                   type="button"
@@ -58,7 +99,7 @@ export const Login = () => {
             <div className="flex items-center justify-between">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
+                type="submit"
               >
                 Login
               </button>
