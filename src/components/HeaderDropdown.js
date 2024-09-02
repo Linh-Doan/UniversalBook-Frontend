b3a71d4729c63bb6092d4aa293f8c5af1ed1f9e0
Linh Doan <thaolinh100798@gmail.com>
2024-09-02 12:10:01 +1000

    register using axios

--- src/components/HeaderDropdown.js
+++ src/components/HeaderDropdown.js
@@ -2,19 +2,33 @@ import { useEffect, useState } from 'react';
 import { getCurrentUser } from '../services/authService';
 import { DropdownLoggedIn } from './DropdownLoggedIn';
 import { DropdownLoggedOut } from './DropdownLoggedOut';
+import { Loading } from './Loading';
 
 export const HeaderDropdown = ({setDropdownVisible}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function fetchUser(){
             const data = await getCurrentUser();
+            setLoading(false);
             setUser(data);
         }
         fetchUser();
-    }, [])
+    }, []);
   return (
     <div>
-        {user ? <DropdownLoggedIn setDropdownVisible={setDropdownVisible} currentUser={user}/> : <DropdownLoggedOut setDropdownVisible={setDropdownVisible} /> }
+        { loading? (
+            <Loading/>
+        ) : user ? (
+            <DropdownLoggedIn 
+                setDropdownVisible={setDropdownVisible} 
+                currentUser={user}
+            />
+        ) : (
+            <DropdownLoggedOut 
+                setDropdownVisible={setDropdownVisible}
+            />
+        )}
     </div>
   )
 }
