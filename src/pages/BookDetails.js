4bf1c56e05cc8bb04383a92e33a03df061ffea74
Linh Doan <thaolinh100798@gmail.com>
2024-09-17 16:11:06 +1000

    ratings actually working

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -99,6 +99,16 @@ export const BookDetails = () => {
         navigate(`/books/${id}/chapters`);
     };
 
+    const calculatingRating = () => {
+        if (commentList.length === 0) {
+            return 0
+        } else {
+            const total = commentList.reduce((n, {book_rating}) => n + book_rating, 0);
+            return Math.round(total/commentList.length * 100) / 100
+        }
+
+    }
+
     return (
         <main>
             {book ? 
@@ -148,9 +158,9 @@ export const BookDetails = () => {
                             </p>
                             <div className="flex items-center">
                                 <svg aria-hidden="true" className="w-5 h-5 text-grey-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                                <p className="ml-2 text-gray-900">{book.rating}/5</p>
+                                <p className="ml-2 text-gray-900">{calculatingRating()}/5</p>
                                 <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full"></span>
-                                <span className="text-gray-900">{book.rating_count} reviews</span>
+                                <span className="text-gray-900">{commentList.length} reviews</span>
                             </div>
                         </div>
                     </div>
