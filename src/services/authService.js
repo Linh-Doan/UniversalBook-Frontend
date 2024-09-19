32eb6d326b8961c3c8a2767c4c5644168fbadd9d
Linh Doan <thaolinh100798@gmail.com>
2024-09-20 07:55:57 +1000

    login error

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,26 +1,47 @@
 import axiosInstance from "../api/axiosInstance";
 import { endpoints } from '../config.js';
+
+export class AuthError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'AuthError';
+    }
+  }
+
 export async function login(loginDetails){
-    await axiosInstance.post(`${endpoints.login}`, 
+    try{
+        await axiosInstance.post(`${endpoints.login}`, 
         loginDetails,
         {
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
             }
         }
-    );
+        );
+    } catch (error) {
+        if (error.response.status === 401) {
+            throw new AuthError('Authentication failed: Invalid credentials');
+        } else {
+            throw error;
+        }
+    }
 }
 
 export async function register(authDetails){
-    await axiosInstance.post(
-        `${endpoints.signup}`,
-        authDetails, 
-        {
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
+    try {
+        await axiosInstance.post(
+            `${endpoints.signup}`,
+            authDetails, 
+            {
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
             }
-        }
-    )
+        );
+    } catch (error) {
+        throw error;
+    }
+    
 }
 
 export async function logout(){
